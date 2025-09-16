062764ff5af9b20467ed6a22effee601e06a13bf
Pallavi Kumari <118119181+Itspallavihithub@users.noreply.github.com>
2025-09-16 10:43:27 +0530

    Update LandingPage.jsx

--- interview-prep-ai/src/pages/LandingPage.jsx
+++ interview-prep-ai/src/pages/LandingPage.jsx
@@ -140,7 +140,7 @@ const LandingPage = () => {
         </div>
 
         <div className="text-sm bg-gray-50 text-secondary text-center p-5 mt-5">
-          Made with ❤️... Happy Coding
+          Made with Pallavi Kashyap... Happy Coding 🚀
         </div>
       </div>
 
